06f8845bb229c9c61e9fe5344cdaf2ccc36483cb
Poon <ivanholin479@gmail.com>
2023-03-22 15:28:10 +0500

    Initial commit

--- src/main/java/Poon/Main.java
+++ src/main/java/Poon/Main.java
@@ -7,26 +7,26 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-//        System.setProperty("webdriver.chrome.driver", "selenium\\chromedriver.exe");
-//
-//        Parser parser = new Parser();
-//        parser.openURL();
-//
-//        List<Playlist> playlists = parser.getPlaylists();
-//        for (int i = 0; i < playlists.size(); i++) {
-//            System.out.println((i + 1) + ": " + playlists.get(i));
-//        }
-//
-//        List<Track> playlist = parser.parserPosts(parser.getOnePlaylist(playlists));
-//        for (Track track : playlist) {
-//            System.out.println(track);
-//        }
+        System.setProperty("webdriver.chrome.driver", "selenium\\chromedriver.exe");
+
+        Parser parser = new Parser();
+        parser.openURL();
+
+        List<Playlist> playlists = parser.getPlaylists();
+        for (int i = 0; i < playlists.size(); i++) {
+            System.out.println((i + 1) + ": " + playlists.get(i));
+        }
+
+        List<Track> playlist = parser.parserPosts(parser.getOnePlaylist(playlists));
+        for (Track track : playlist) {
+            System.out.println(track);
+        }
 
         XML xml = new XML();
-//        xml.createXml(playlist);
+        xml.createXml(playlist);
         xml.findTrack("Kordhell");
         xml.findTrack("Hensonn");
-//        xml.addXmlNote("Culture Shock", "Discotheque", "123");
-//        xml.addXmlNote("Feint", "Homebound", "321");
+        xml.addXmlNote("Culture Shock", "Discotheque", "123");
+        xml.addXmlNote("Feint", "Homebound", "321");
     }
 }
